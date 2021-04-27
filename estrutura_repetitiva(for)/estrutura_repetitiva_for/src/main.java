55b7cc30e548686bd33e4a67bdacf4c3004ec65f
Kleber Henrique Felix Dos Santos <79463086+kleubinho@users.noreply.github.com>
2021-04-27 10:04:11 -0300

    estrutura repetitiva

---
+++ estrutura_repetitiva(for)/estrutura_repetitiva_for/src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// Sintaxe/regra "para" (for)
+		//for (inicio; condição; encremento){
+		//	comando 1
+		//	comando 2
+		//}
+		
+		for (int i = 4; i >= 0; i--) {
+		System.out.println("Valor de i: " + i);	
+		}
+		
+	}
+
+}
